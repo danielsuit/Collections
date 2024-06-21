87fb2a46b2b56297684f22e31beeacceb7355bb8
Daniel Suit <106452651+danielsuit@users.noreply.github.com>
2024-06-21 17:09:05 -0400

    Initial Commit

---
+++ Collections/CollectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionsApp.swift
+//  Collections
+//
+//  Created by Daniel Suit on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CollectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
